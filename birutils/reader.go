6cc5aff8462c4fe6a8ea0bd72ea18e6245987226
Sithija Nelusha Silva <hello@snelusha.dev>
2026-01-29 10:05:32 +0530

    all done

--- birutils/reader.go
+++ birutils/reader.go
@@ -248,6 +248,36 @@ func (r *BIRReader) getIntegerFromCP(index int) int64 {
 	return 0
 }
 
+func (r *BIRReader) getByteFromCP(index int) uint8 {
+	if index < 0 || index >= len(r.cp) {
+		return 0
+	}
+	if val, ok := r.cp[index].(uint8); ok {
+		return val
+	}
+	return 0
+}
+
+func (r *BIRReader) getFloatFromCP(index int) float64 {
+	if index < 0 || index >= len(r.cp) {
+		return 0
+	}
+	if val, ok := r.cp[index].(float64); ok {
+		return val
+	}
+	return 0
+}
+
+func (r *BIRReader) getBooleanFromCP(index int) bool {
+	if index < 0 || index >= len(r.cp) {
+		return false
+	}
+	if val, ok := r.cp[index].(bool); ok {
+		return val
+	}
+	return false
+}
+
 func (br *BIRReader) readImports() ([]bir.BIRImportModule, error) {
 	var count int32
 	if err := binary.Read(br.r, binary.BigEndian, &count); err != nil {
@@ -458,7 +488,6 @@ func (br *BIRReader) readFunctions() ([]bir.BIRFunction, error) {
 		if err := binary.Read(br.r, binary.BigEndian, &requiredParamsCount); err != nil {
 			return nil, err
 		}
-		fmt.Printf("[READER] REQUIRED PARAMS COUNT: %d IN FUNCTION %s\n", requiredParamsCount, name)
 
 		requiredParams := make([]bir.BIRParameter, requiredParamsCount)
 		for j := 0; j < int(requiredParamsCount); j++ {
@@ -467,7 +496,6 @@ func (br *BIRReader) readFunctions() ([]bir.BIRFunction, error) {
 				return nil, err
 			}
 			paramName := model.Name(br.getStringFromCP(int(paramNameIdx)))
-			fmt.Printf("[READER] PARAM NAME: %s\n", paramName)
 
 			var paramFlags int64
 			if err := binary.Read(br.r, binary.BigEndian, &paramFlags); err != nil {
@@ -522,55 +550,395 @@ func (br *BIRReader) readFunctions() ([]bir.BIRFunction, error) {
 			}
 		}
 
-		// var localVarCount int32
-		// if err := binary.Read(br.r, binary.BigEndian, &localVarCount); err != nil {
-		// 	return nil, err
-		// }
+		var localVarCount int32
+		if err := binary.Read(br.r, binary.BigEndian, &localVarCount); err != nil {
+			return nil, err
+		}
 
-		// localVars := make([]bir.BIRVariableDcl, localVarCount)
-		// for range localVars {
-		// 	var localVarKind uint8
-		// 	if err := binary.Read(br.r, binary.BigEndian, &localVarKind); err != nil {
-		// 		return nil, err
-		// 	}
-		//
-		// 	var localVarTypeIdx int32
-		// 	if err := binary.Read(br.r, binary.BigEndian, &localVarTypeIdx); err != nil {
-		// 		return nil, err
-		// 	}
-		// 	localVarType := br.getTypeFromCP(int(localVarTypeIdx))
-		//
-		// 	var localVarNameIdx int32
-		// 	if err := binary.Read(br.r, binary.BigEndian, &localVarNameIdx); err != nil {
-		// 		return nil, err
-		// 	}
-		// 	localVarName := model.Name(br.getStringFromCP(int(localVarNameIdx)))
-		//
-		// 	localVar := bir.BIRVariableDcl{
-		// 		Kind: bir.VarKind(localVarKind),
-		// 		Name: localVarName,
-		// 		Type: localVarType,
-		// 	}
+		localVars := make([]bir.BIRVariableDcl, localVarCount)
 
-		// fmt.Printf("[READER] LOCAL VAR: %+v\n", localVar)
+		for j := 0; j < int(localVarCount); j++ {
+			var localVarKind uint8
+			if err := binary.Read(br.r, binary.BigEndian, &localVarKind); err != nil {
+				return nil, err
+			}
 
-		// if localVarKind == uint8(bir.VAR_KIND_ARG) {
-		// 	fmt.Printf("[READER] KIND ARG\n")
-		//
-		// 	var metaVarNameIdx int32
-		// 	if err := binary.Read(br.r, binary.BigEndian, &metaVarNameIdx); err != nil {
-		// 		return nil, err
-		// 	}
-		// 	metaVarName := br.getStringFromCP(int(metaVarNameIdx))
-		// 	localVar.MetaVarName = metaVarName
-		// }
+			var localVarTypeIdx int32
+			if err := binary.Read(br.r, binary.BigEndian, &localVarTypeIdx); err != nil {
+				return nil, err
+			}
+			localVarType := br.getTypeFromCP(int(localVarTypeIdx))
 
-		// localVars = append(localVars, localVar)
-		// }
-		// function.LocalVars = localVars
+			var localVarNameIdx int32
+			if err := binary.Read(br.r, binary.BigEndian, &localVarNameIdx); err != nil {
+				return nil, err
+			}
+			localVarName := model.Name(br.getStringFromCP(int(localVarNameIdx)))
+
+			localVar := bir.BIRVariableDcl{
+				Kind: bir.VarKind(localVarKind),
+				Name: localVarName,
+				Type: localVarType,
+			}
+
+			if localVarKind == uint8(bir.VAR_KIND_ARG) {
+				var metaVarNameIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &metaVarNameIdx); err != nil {
+					return nil, err
+				}
+				metaVarName := br.getStringFromCP(int(metaVarNameIdx))
+				localVar.MetaVarName = metaVarName
+			} else if localVarKind == uint8(bir.VAR_KIND_LOCAL) {
+				var metaVarNameIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &metaVarNameIdx); err != nil {
+					return nil, err
+				}
+				metaVarName := br.getStringFromCP(int(metaVarNameIdx))
+				localVar.MetaVarName = metaVarName
+
+				var endBBIdIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &endBBIdIdx); err != nil {
+					return nil, err
+				}
+				endBBId := model.Name(br.getStringFromCP(int(endBBIdIdx)))
+				localVar.EndBB = &bir.BIRBasicBlock{
+					Id: endBBId,
+				}
+
+				var startBBIdIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &startBBIdIdx); err != nil {
+					return nil, err
+				}
+				startBBId := model.Name(br.getStringFromCP(int(startBBIdIdx)))
+				localVar.StartBB = &bir.BIRBasicBlock{
+					Id: startBBId,
+				}
+
+				var insOffset int32
+				if err := binary.Read(br.r, binary.BigEndian, &insOffset); err != nil {
+					return nil, err
+				}
+				localVar.InsOffset = int(insOffset)
+			}
+
+			localVars[j] = localVar
+		}
+		function.LocalVars = localVars
+
+		var basicBlockCount int32
+		if err := binary.Read(br.r, binary.BigEndian, &basicBlockCount); err != nil {
+			return nil, err
+		}
+
+		basicBlocks := make([]bir.BIRBasicBlock, basicBlockCount)
+		for j := 0; j < int(basicBlockCount); j++ {
+			var idIdx int32
+			if err := binary.Read(br.r, binary.BigEndian, &idIdx); err != nil {
+				return nil, err
+			}
+			id := model.Name(br.getStringFromCP(int(idIdx)))
+
+			block := bir.BIRBasicBlock{
+				Id: id,
+			}
+
+			var instructionCount int32
+			if err := binary.Read(br.r, binary.BigEndian, &instructionCount); err != nil {
+				return nil, err
+			}
+
+			instructions := make([]bir.BIRInstruction, instructionCount)
+			for k := 0; k < int(instructionCount); k++ {
+				var insKind uint8
+				if err := binary.Read(br.r, binary.BigEndian, &insKind); err != nil {
+					return nil, err
+				}
+				instructionKind := bir.InstructionKind(insKind)
+
+				switch instructionKind {
+				case bir.INSTRUCTION_KIND_MOVE:
+					rhsOp, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					lhsOp, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					instructions[k] = &bir.Move{
+						BIRInstructionBase: bir.BIRInstructionBase{
+							LhsOp: lhsOp,
+						},
+						RhsOp: rhsOp,
+					}
+				case bir.INSTRUCTION_KIND_ADD, bir.INSTRUCTION_KIND_SUB, bir.INSTRUCTION_KIND_MUL, bir.INSTRUCTION_KIND_DIV, bir.INSTRUCTION_KIND_MOD, bir.INSTRUCTION_KIND_EQUAL, bir.INSTRUCTION_KIND_NOT_EQUAL, bir.INSTRUCTION_KIND_GREATER_THAN, bir.INSTRUCTION_KIND_GREATER_EQUAL, bir.INSTRUCTION_KIND_LESS_THAN, bir.INSTRUCTION_KIND_LESS_EQUAL, bir.INSTRUCTION_KIND_AND, bir.INSTRUCTION_KIND_OR, bir.INSTRUCTION_KIND_REF_EQUAL, bir.INSTRUCTION_KIND_REF_NOT_EQUAL, bir.INSTRUCTION_KIND_CLOSED_RANGE, bir.INSTRUCTION_KIND_HALF_OPEN_RANGE, bir.INSTRUCTION_KIND_ANNOT_ACCESS, bir.INSTRUCTION_KIND_BITWISE_AND, bir.INSTRUCTION_KIND_BITWISE_OR, bir.INSTRUCTION_KIND_BITWISE_XOR, bir.INSTRUCTION_KIND_BITWISE_LEFT_SHIFT, bir.INSTRUCTION_KIND_BITWISE_RIGHT_SHIFT, bir.INSTRUCTION_KIND_BITWISE_UNSIGNED_RIGHT_SHIFT:
+					rhsOp1, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					rhsOp2, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					lhsOp, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					instructions[k] = &bir.BinaryOp{
+						BIRInstructionBase: bir.BIRInstructionBase{
+							LhsOp: lhsOp,
+						},
+						Kind:   instructionKind,
+						RhsOp1: *rhsOp1,
+						RhsOp2: *rhsOp2,
+					}
+				case bir.INSTRUCTION_KIND_TYPEOF, bir.INSTRUCTION_KIND_NOT, bir.INSTRUCTION_KIND_NEGATE:
+					rhsOp, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					lhsOp, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					instructions[k] = &bir.UnaryOp{
+						BIRInstructionBase: bir.BIRInstructionBase{
+							LhsOp: lhsOp,
+						},
+						Kind:  instructionKind,
+						RhsOp: rhsOp,
+					}
+				case bir.INSTRUCTION_KIND_CONST_LOAD:
+					var constLoadTypeIdx int32
+					if err := binary.Read(br.r, binary.BigEndian, &constLoadTypeIdx); err != nil {
+						return nil, err
+					}
+					constLoadType := br.getTypeFromCP(int(constLoadTypeIdx))
+
+					lhsOp, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+
+					var value any
+
+					switch constLoadType.BTypeGetTag() {
+					case model.TypeTags_INT, model.TypeTags_SIGNED32_INT, model.TypeTags_SIGNED16_INT, model.TypeTags_SIGNED8_INT, model.TypeTags_UNSIGNED32_INT, model.TypeTags_UNSIGNED16_INT, model.TypeTags_UNSIGNED8_INT:
+						var valueIdx int32
+						if err := binary.Read(br.r, binary.BigEndian, &valueIdx); err != nil {
+							return nil, err
+						}
+						value = br.getIntegerFromCP(int(valueIdx))
+					case model.TypeTags_BYTE:
+						var valueIdx int32
+						if err := binary.Read(br.r, binary.BigEndian, &valueIdx); err != nil {
+							return nil, err
+						}
+						value = br.getByteFromCP(int(valueIdx))
+					case model.TypeTags_FLOAT:
+						var valueIdx int32
+						if err := binary.Read(br.r, binary.BigEndian, &valueIdx); err != nil {
+							return nil, err
+						}
+						value = br.getFloatFromCP(int(valueIdx))
+					case model.TypeTags_STRING, model.TypeTags_CHAR_STRING, model.TypeTags_DECIMAL:
+						var valueIdx int32
+						if err := binary.Read(br.r, binary.BigEndian, &valueIdx); err != nil {
+							return nil, err
+						}
+						value = br.getStringFromCP(int(valueIdx))
+					case model.TypeTags_BOOLEAN:
+						var valueIdx int32
+						if err := binary.Read(br.r, binary.BigEndian, &valueIdx); err != nil {
+							return nil, err
+						}
+						value = br.getBooleanFromCP(int(valueIdx))
+					}
+
+					instructions[k] = &bir.ConstantLoad{
+						BIRInstructionBase: bir.BIRInstructionBase{
+							LhsOp: lhsOp,
+						},
+						Type:  constLoadType,
+						Value: value,
+					}
+				}
+			}
+			block.Instructions = instructions
+
+			var terminatorKind uint8
+			if err := binary.Read(br.r, binary.BigEndian, &terminatorKind); err != nil {
+				return nil, err
+			}
+
+			termInstructionKind := bir.InstructionKind(terminatorKind)
+
+			switch termInstructionKind {
+			case bir.INSTRUCTION_KIND_RETURN:
+				fmt.Println("Reading RETURN")
+				block.Terminator = &bir.Return{}
+			case bir.INSTRUCTION_KIND_GOTO:
+				fmt.Println("Reading GOTO")
+				var idIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &idIdx); err != nil {
+					return nil, err
+				}
+				id := br.getStringFromCP(int(idIdx))
+				block.Terminator = &bir.Goto{
+					BIRTerminatorBase: bir.BIRTerminatorBase{
+						ThenBB: &bir.BIRBasicBlock{
+							Id: model.Name(id),
+						},
+					},
+				}
+			case bir.INSTRUCTION_KIND_BRANCH:
+				fmt.Println("Reading BRANCH")
+
+				op, err := br.readOperand()
+				if err != nil {
+					return nil, err
+				}
+
+				var trueBBIdIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &trueBBIdIdx); err != nil {
+					return nil, err
+				}
+				trueBBId := br.getStringFromCP(int(trueBBIdIdx))
+
+				var falseBBIdIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &falseBBIdIdx); err != nil {
+					return nil, err
+				}
+				falseBBId := br.getStringFromCP(int(falseBBIdIdx))
+
+				block.Terminator = &bir.Branch{
+					Op: op,
+					TrueBB: &bir.BIRBasicBlock{
+						Id: model.Name(trueBBId),
+					},
+					FalseBB: &bir.BIRBasicBlock{
+						Id: model.Name(falseBBId),
+					},
+				}
+			case bir.INSTRUCTION_KIND_CALL:
+				var isVirtual bool
+				if err := binary.Read(br.r, binary.BigEndian, &isVirtual); err != nil {
+					return nil, err
+				}
+
+				var pkgIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &pkgIdx); err != nil {
+					return nil, err
+				}
+				pkg := br.getPackageFromCP(int(pkgIdx))
+
+				var nameIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &nameIdx); err != nil {
+					return nil, err
+				}
+				name := br.getStringFromCP(int(nameIdx))
+
+				var argsCount int32
+				if err := binary.Read(br.r, binary.BigEndian, &argsCount); err != nil {
+					return nil, err
+				}
+
+				args := make([]bir.BIROperand, argsCount)
+				for k := 0; k < int(argsCount); k++ {
+					arg, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					args[k] = *arg
+				}
+
+				var lshOpExists bool
+				if err := binary.Read(br.r, binary.BigEndian, &lshOpExists); err != nil {
+					return nil, err
+				}
+
+				var lhsOp *bir.BIROperand
+				if lshOpExists {
+					op, err := br.readOperand()
+					if err != nil {
+						return nil, err
+					}
+					lhsOp = op
+				}
+
+				var thenBBIdIdx int32
+				if err := binary.Read(br.r, binary.BigEndian, &thenBBIdIdx); err != nil {
+					return nil, err
+				}
+				thenBBId := br.getStringFromCP(int(thenBBIdIdx))
+
+				block.Terminator = &bir.Call{
+					Kind:      termInstructionKind,
+					IsVirtual: isVirtual,
+					CalleePkg: pkg,
+					Name:      model.Name(name),
+					Args:      args,
+					BIRTerminatorBase: bir.BIRTerminatorBase{
+						ThenBB: &bir.BIRBasicBlock{
+							Id: model.Name(thenBBId),
+						},
+						BIRInstructionBase: bir.BIRInstructionBase{
+							LhsOp: lhsOp,
+						},
+					},
+				}
+			}
+
+			basicBlocks[j] = block
+		}
+		function.BasicBlocks = basicBlocks
 
 		functions[i] = function
 	}
 
 	return functions, nil
+}
+
+func (br *BIRReader) readOperand() (*bir.BIROperand, error) {
+	var ignoreVariable bool
+	if err := binary.Read(br.r, binary.BigEndian, &ignoreVariable); err != nil {
+		return nil, err
+	}
+
+	if ignoreVariable {
+		var varTypeIdx int32
+		if err := binary.Read(br.r, binary.BigEndian, &varTypeIdx); err != nil {
+			return nil, err
+		}
+		varType := br.getTypeFromCP(int(varTypeIdx))
+		return &bir.BIROperand{
+			VariableDcl: &bir.BIRVariableDcl{
+				Type: varType,
+			},
+		}, nil
+	}
+
+	var varKind uint8
+	if err := binary.Read(br.r, binary.BigEndian, &varKind); err != nil {
+		return nil, err
+	}
+
+	var scope uint8
+	if err := binary.Read(br.r, binary.BigEndian, &scope); err != nil {
+		return nil, err
+	}
+
+	var nameIdx int32
+	if err := binary.Read(br.r, binary.BigEndian, &nameIdx); err != nil {
+		return nil, err
+	}
+	name := model.Name(br.getStringFromCP(int(nameIdx)))
+
+	return &bir.BIROperand{
+		VariableDcl: &bir.BIRVariableDcl{
+			Kind:  bir.VarKind(varKind),
+			Scope: bir.VarScope(scope),
+			Name:  name,
+		},
+	}, nil
 }
