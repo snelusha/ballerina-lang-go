dd7d82f4895743118719938e8c2f8524172cd217
Sithija Nelusha Silva <hello@snelusha.dev>
2026-02-17 15:41:44 +0530

    Fix cycle detection in type resolver

--- semantics/type_resolver.go
+++ semantics/type_resolver.go
@@ -17,15 +17,16 @@
 package semantics
 
 import (
+	"fmt"
+	"math/big"
+	"math/bits"
+	"strconv"
+
 	"ballerina-lang-go/ast"
 	"ballerina-lang-go/context"
 	"ballerina-lang-go/model"
 	"ballerina-lang-go/semtypes"
 	"ballerina-lang-go/tools/diagnostics"
-	"fmt"
-	"math/big"
-	"math/bits"
-	"strconv"
 )
 
 type (
@@ -228,6 +229,7 @@ func (t *TypeResolver) resolveTypeDefinition(defn *ast.BLangTypeDefinition, dept
 	}
 	if depth == defn.CycleDepth {
 		t.ctx.SemanticError(fmt.Sprintf("invalid cycle detected for type definition %s", defn.Name.GetValue()), defn.GetPosition())
+		return nil
 	}
 	defn.CycleDepth = depth
 	semType := t.resolveBType(defn.GetTypeData().TypeDescriptor.(ast.BType), depth)
